284821951f4abf21f07dcaa14fb8b65b1bc8ff39
Rustamnurg <rustik6030@gmail.com>
2016-08-19 12:52:14 +0300

    Create

---
+++ Taskmanager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Taskmanager
+//
+//  Created by Rustam N on 19.08.16.
+//  Copyright © 2016 Rustam N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Taskmanager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Taskmanager
+//
+//  Created by Rustam N on 19.08.16.
+//  Copyright © 2016 Rustam N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
